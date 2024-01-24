3848f19bd2616276e94748dfa3fc654749206bb8
cocozer <corentin.pak@gmail.com>
2024-01-24 17:47:42 +0100

    Ajout de la définition de la classe boids dans le fichier boids.hpp avec quelques attributs et constructeurs de base

--- include/boids.hpp
+++ include/boids.hpp
@@ -1,2 +1,17 @@
 #pragma once
-// test branches boids
+
+namespace boids {
+class Boids {
+private:
+    float size;
+    float x;
+    float y;
+    float vx;
+    float vy;
+
+public:
+    Boids();                                     // Default constructor
+    Boids(float x, float y);                     // Constructor by position
+    Boids(float x, float y, float vx, float vy); // Constructor by position and velocity
+};
+} // namespace boids
