54e967fd49eee7c54a5d7bd60fc66329be74fd6a
cocozer <corentin.pak@gmail.com>
2024-01-24 18:02:03 +0100

    formatage du code

--- src/boids.cpp
+++ src/boids.cpp
@@ -5,11 +5,11 @@ Boids::Boids()
     : size(0.1), x(0.0), y(0.0), vx(0.0), vy(0.0)
 {}
 
-Boids::Boids(float x, float y)
+Boids::Boids(const float& x, const float& y)
     : size(0.1), x(x), y(y), vx(0.0), vy(0.0)
 {}
 
-Boids::Boids(float x, float y, float vx, float vy)
+Boids::Boids(const float& x, const float& y, const float& vx, const float& vy)
     : size(0.1), x(x), y(y), vx(vx), vy(vy)
 {}
 } // namespace boids
