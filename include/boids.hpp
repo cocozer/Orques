2e111ae04d488eab8ca91dc342ca08cd84dae2dd
salomaxx <teyssier.salome.78@gmail.com>
2024-01-31 15:01:41 +0100

    ajout de attribut acceleration et methodes dans la classe boids

--- include/boids.hpp
+++ include/boids.hpp
@@ -8,10 +8,16 @@ private:
     float y;
     float vx;
     float vy;
+    float ax;
+    float ay;
 
 public:
     Boids();                                                                 // Default constructor
     Boids(const float& x, const float& y);                                   // Constructor by position
     Boids(const float& x, const float& y, const float& vx, const float& vy); // Constructor by position and velocity
+
+    void UpdatePosition();
+    void UpdateVelocity();
+
 };
 } // namespace boids
