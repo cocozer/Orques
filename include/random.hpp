95c5c5d55e4f9bf37895933a770a078d3958336a
salomaxx <teyssier.salome.78@gmail.com>
2024-01-31 16:08:46 +0100

    création du fichier hpp et cpp random

---
+++ include/random.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <cstdio>
+#include <cstdlib>
+#include <thread>
+#include <ctime>
+
+namespace randgen { 
+
+    double Rand01();
+
+}
---
+++ src/random.cpp
@@ -0,0 +1,13 @@
+#include "random.hpp"
+
+namespace randgen {
+
+double Rand01()
+{
+    thread_local std::default_random_engine gen{std::random_device{}()};
+    thread_local auto                       distrib = std::uniform_real_distribution<double>{0.0, 1.0};
+
+    return distrib(gen);
+}
+
+} // namespace randgen
