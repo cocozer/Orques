4da39c23a83619cb9661da1f2b00e279a845761a
cocozer <corentin.pak@gmail.com>
2024-02-07 17:52:45 +0100

    ajout d'assert dans flock pour gérer les erreurs à la suppression d'un boid, simplification du constructeur en utilisant le constructeur par défaut de boid

--- src/flock.cpp
+++ src/flock.cpp
@@ -1,18 +1,11 @@
 #include "flock.hpp"
+#include <cassert>
 
 namespace boids {
 
 Flock::Flock(int n)
+    : flock(n)
 {
-    // Crée n boids avec des positions et des vitesses aléatoires, et les ajoute au vecteur de boids
-    for (int i = 0; i < n; ++i)
-    {
-        float x  = 0;
-        float y  = 0;
-        float vx = 0;
-        float vy = 0;
-        flock.emplace_back(x, y, vx, vy);
-    }
 }
 
 void Flock::AddBoid(const Boid& boid)
@@ -25,6 +18,10 @@ void Flock::RemoveBoid(int index)
     if (index >= 0 && index < flock.size())
     {
         flock.erase(flock.begin() + index);
+    }
+    else
+    {
+        assert(false);
     }
 }
 
