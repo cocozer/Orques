5168e0212018a60bfe596be507c63d315c311c4d
cocozer <corentin.pak@gmail.com>
2024-02-07 16:24:10 +0100

    séparation de classes boid / boids pour gérer l'entièreté des boids

--- include/boids.hpp
+++ include/boids.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <vector>
+
 namespace boids {
-class Boids {
+class Boid {
 private:
     float size;
     float x;
@@ -12,12 +14,30 @@ private:
     float ay;
 
 public:
-    Boids();                                                                 // Default constructor
-    Boids(const float& x, const float& y);                                   // Constructor by position
-    Boids(const float& x, const float& y, const float& vx, const float& vy); // Constructor by position and velocity
+    Boid();                                                                 // Default constructor
+    Boid(const float& x, const float& y);                                   // Constructor by position
+    Boid(const float& x, const float& y, const float& vx, const float& vy); // Constructor by position and velocity
 
     void UpdatePosition();
-    void UpdateVelocity();
+    // void UpdateVelocity();
+};
 
+class Boids {
+private:
+    std::vector<Boid> boids; // Vecteur de boids
+
+public:
+    Boids(int n); // Constructeur pour créer n boids
+
+    // Méthodes pour manipuler le vecteur de boids
+    void        AddBoid(const Boid& boid);
+    void        RemoveBoid(int index);
+    Boid&       GetBoid(int index);
+    const Boid& GetBoid(int index) const;
+    int         NumBoids() const;
+
+    // Méthodes pour mettre à jour tous les boids dans le vecteur
+    void UpdatePositions();
+    // void UpdateVelocities();
 };
 } // namespace boids
--- src/boids.cpp
+++ src/boids.cpp
@@ -1,27 +1,75 @@
 #include "boids.hpp"
 
 namespace boids {
-Boids::Boids()
+Boid::Boid()
     : size(0.1), x(0.0), y(0.0), vx(0.0), vy(0.0)
 {}
 
-Boids::Boids(const float& x, const float& y)
+Boid::Boid(const float& x, const float& y)
     : size(0.1), x(x), y(y), vx(0.0), vy(0.0)
 {}
 
-Boids::Boids(const float& x, const float& y, const float& vx, const float& vy)
+Boid::Boid(const float& x, const float& y, const float& vx, const float& vy)
     : size(0.1), x(x), y(y), vx(vx), vy(vy)
 {}
 
-void Boids::UpdatePosition()
+void Boid::UpdatePosition()
 {
     x += vx;
     y += vy;
 }
 
-// void Boids::UpdateVelocity()
-// {
+Boids::Boids(int n)
+{
+    // Crée n boids avec des positions et des vitesses aléatoires, et les ajoute au vecteur de boids
+    for (int i = 0; i < n; ++i)
+    {
+        float x  = 0;
+        float y  = 0;
+        float vx = 0;
+        float vy = 0;
+        boids.emplace_back(x, y, vx, vy);
+    }
+}
 
+void Boids::AddBoid(const Boid& boid)
+{
+    boids.push_back(boid);
+}
+
+void Boids::RemoveBoid(int index)
+{
+    if (index >= 0 && index < boids.size())
+    {
+        boids.erase(boids.begin() + index);
+    }
+}
+
+Boid& Boids::GetBoid(int index)
+{
+    return boids[index];
+}
+
+const Boid& Boids::GetBoid(int index) const
+{
+    return boids[index];
+}
+
+int Boids::NumBoids() const
+{
+    return boids.size();
+}
+
+void Boids::UpdatePositions()
+{
+    for (auto& boid : boids)
+    {
+        boid.UpdatePosition();
+    }
+}
+
+// void Boids::UpdateVelocities() {
+//     // À implémenter
 // }
 
 } // namespace boids
