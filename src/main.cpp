05a556448c4ad7f74cb7f3224c67f67acc1cd888
lagardeheidi <lagarde.heidi34@gmail.com>
2024-03-25 15:19:46 +0100

    commentaires

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,19 @@
 #include <interface.hpp>
 #include "doctest/doctest.h"
 
+// #define TINYOBJLOADER_IMPLEMENTATION
+// #include <tiny_obj_loader.h>
+
 int main()
 {
     Interface interface; // on créé l'objet interface de la classe Interface
+
+    // passer un model 3d a run update loop
+    // ici definir model (shader, mv matrix etc et obj 3d)
+
+    // load shader (creer shader vs fs)
+    //     p6::Shader Shader = p6::load_shader("shaders/3D.vs.glsl", "shaders/text3D.fs.glsl");
+
     interface.run_update_loop();
     return 0;
 }
